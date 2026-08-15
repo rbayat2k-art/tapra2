89064233582c0530b8309046005981da50a643d7
rbayat2k-art <rbayat2k@gmail.com>
2026-08-15 17:50:37 +0330

    fix: حفظ سازگاری رویدادهای تاریخچه فروش

--- server/migrations/0012_customer_identity_timeline_events.sql
+++ server/migrations/0012_customer_identity_timeline_events.sql
@@ -5,5 +5,6 @@ ALTER TABLE customer_timeline_events
   ADD CONSTRAINT customer_timeline_events_event_type_check CHECK (event_type IN (
     'customer_created', 'phone_added', 'address_added', 'source_linked',
     'customer_merged', 'customer_split', 'customer_imported', 'import_data_linked',
+    'sales_lead_created', 'sales_call_logged', 'sales_marketing_linked',
     'customer_identity_merged', 'customer_identity_split'
   ));
